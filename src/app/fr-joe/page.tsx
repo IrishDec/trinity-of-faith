56b8b576e56de80dde65ae125f065b1fc136ff58
Declan <declanobrien77@gmail.com>
2026-05-29 22:06:05 +0000

    force dynamic fr joe page

--- src/app/fr-joe/page.tsx
+++ src/app/fr-joe/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { supabase } from "@/lib/supabaseClient";
 import SiteFooter from "@/components/SiteFooter";
 import SiteHeader from "@/components/SiteHeader";
